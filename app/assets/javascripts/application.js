e76742ac1359ab37a4dfb3a070bcc8f5e19ddc40
William Holt <wtholt27@gmail.com>
2015-03-09 17:49:40 -0400

    setting up environment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-route
+//= require underscore
+//= require home
 //= require_tree .
