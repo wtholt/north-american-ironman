74b591ca373f90d39633398f26f6a98ac05d9de2
William Holt <wtholt27@gmail.com>
2015-03-15 18:02:45 -0400

    testing, added simplecov gem

--- Gemfile
+++ Gemfile
@@ -59,6 +59,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'simplecov', :require => false, :group => :test
+
+
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,4 +2,99 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
 
+  let!(:group){
+    FactoryGirl.create(:group)
+  }
+  let!(:event){
+    FactoryGirl.create(:event)
+  }
+
+  describe 'GET #index' do
+    it 'should return all events' do
+      get :index, format: :json, group_id: group
+      expect(assigns(:events)).not_to eq(nil)
+    end
+  end
+
+  describe 'GET #show' do 
+    it 'should show an event object' do 
+      get :show, format: :json, id: event, group_id: group
+      expect(assigns(:event)).to eq event
+      expect(assigns(:event)).not_to eq(nil)
+    end
+  end
+
+  describe 'GET #new' do 
+    it 'should initialize a new event' do 
+      get :new, format: :json, id: event, group_id: group
+      expect(assigns(:event).class).to eq Event
+      expect(assigns(:event)).not_to eq nil
+      expect(assigns(:event).new_record?).to eq(true)
+    end
+  end
+
+  describe 'POST #create' do 
+    context 'with good data' do 
+      it 'should create' do 
+        event_params = FactoryGirl.attributes_for(:event)
+        expect{
+          post :create, format: :json, :event => event_params, group_id: group
+        }.to change(Event, :count).by(1)
+      end
+    end
+    context 'with bad data' do 
+      it 'shouldnt create' do 
+        expect{
+          post :create, format: :json, event:{
+            name: '',
+            info: '',
+            location: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: '',
+          }, group_id: group
+        }.to_not change(Event, :count)
+      end
+    end
+  end
+
+  describe 'GET #edit' do 
+    it 'should edit an event' do 
+      get :edit, format: :json, id: event, group_id: group
+      expect(assigns(:event)).to eq event
+      expect(assigns(:event)).not_to eq(nil)
+    end
+  end
+
+  describe 'PATCH #update' do 
+    context 'with good data' do
+      it 'updates event' do 
+        event_params = FactoryGirl.attributes_for(:event)
+        patch :update, format: :json, id: event, event: event_params, group_id: group
+      end
+    end
+    context 'with bad data' do 
+      it 'doesnt update' do 
+        patch :update, format: :json, id: event, event: {
+          name: '',
+          info: '',
+          location: '',
+          address: '',
+          city: '',
+          state: '',
+          zip: '',
+        }, group_id: group
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do 
+    it 'should delete an event' do 
+      expect{
+        delete :destroy, format: :json, id: event, group_id: group
+      }.to change(Event, :count).by(-1)
+    end
+  end
+
 end
