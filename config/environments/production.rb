8a22eafb25958368ad104f76c2b218cecc755df0
William Holt <wtholt27@gmail.com>
2015-03-17 21:07:33 -0400

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.assets.js_compressor = Uglifier.new(mangle: false)
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
